3c0d54f33c1feeecee311a4d6c3a8f2ab43d3dff
Vijay Vardhan Vasamsetti <106310705+Vijay-159@users.noreply.github.com>
2022-07-11 21:02:38 +0530

    Update 04_manipulate_string.py

--- 04_manipulate_string.py
+++ 04_manipulate_string.py
@@ -3,24 +3,31 @@
 # Manipulate string
 
 '''
-Given a string S and a character C (as string of length 1), return a string with the characters surrounding any occurrence of C reversed. 
-
-For example, if S is "Hello beautiful world" and C is a space, return "Hellb oeautifuw lorld". 
-
+Given a string S and a character C (as string of length 1), return a string with the characters surrounding any occurrence of C reversed.
+For example, if S is "Hello beautiful world" and C is a space, return "Hellb oeautifuw lorld".
 Skip any positions where C occurs twice in a row.
 '''
 import unittest
-def manipulate_string(S,C):
-  i = 0
-  ansst = ""
-  # write your code here
-  return ansst
 
+
+def manipulate_string(S, C):
+  ansst = ""
+  length = len(S)
+  previous = 0
+  temp_s = list(S)
+  while (S.find(C, previous) != -1):
+    index = S.find(C, previous)
+    temp_s[index-1], temp_s[index+1] = temp_s[index+1], temp_s[index-1]
+    previous = index+1
+  for r in temp_s:
+    ansst += r
+  return ansst
 
 
 class Dict_to_list(unittest.TestCase):
   def test_01(self):
-    self.assertEqual(manipulate_string('Hello beautiful world', ' '), 'Hellb oeautifuw lorld')
+    self.assertEqual(manipulate_string(
+        'Hello beautiful world', ' '), 'Hellb oeautifuw lorld')
 
   def test_02(self):
     self.assertEqual(manipulate_string('Hello-World', '-'), 'HellW-oorld')
